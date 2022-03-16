e87d478cebdbefd171f9b25dbaf0aad14ec8c8b5
designerCity <97098829+designerCity@users.noreply.github.com>
2022-03-16 17:31:27 +0900

    Update FoodList.js

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,38 +1,38 @@
-import './FoodList.css'
 
 function formatDate(value) {
-  const date = new Date(value);
-  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
-}
-
-function FoodListItem({ item, onDelete }) {
-
-  const { imgUrl, title, calorie, content, createdAt } = item;
-  const handleDeleteClick = () => onDelete(item.id)
-
- 
-  return (
-    <div className="FoodListItem">
-      <img src={imgUrl} alt={title} />
-      <div>{title}</div>
-      <div>{calorie}</div>
-      <div>{content}</div>
-      <div>{formatDate(createdAt)}</div>
-      <button onClick={handleDeleteClick}>삭제</button>
-    </div>
-  );
-}
-
-function FoodList({ items, onDelete }) {
-  return (
-    <ul className="FoodList">
-      {items.map((item) => (
-        <li>
-          <FoodListItem item={item} onDelete={onDelete} />
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-export default FoodList;
+    const date = new Date(value);
+    return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+  }
+  
+  function FoodListItem({ item, onDelete }) {
+    const { imgUrl, title, calorie, content, createdAt } = item;
+    const handleDeleteClick = () => onDelete(item.id)
+  
+    return (
+      <div className="FoodListItem">
+        <img src={imgUrl} alt={title} />
+        
+          <div>{title}</div>
+          <div>{calorie} kcal </div>
+          <div>{content}</div>
+          <div>{formatDate(createdAt)}</div>
+          <button onClick={handleDeleteClick}>삭제</button>
+      </div>
+    );
+  }
+  
+  function FoodList({ items, onDelete }) {
+    return (
+      <ul>
+        {items.map((item) => {
+          return (
+          <li key={item.id}>
+            <FoodListItem item={item} onDelete={onDelete} />
+          </li>
+          )
+        })}
+      </ul>
+    );
+  }
+  
+  export default FoodList;
