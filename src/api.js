9d4bcc20043fecd0a77953c03f9dca0a8db27148
designerCity <97098829+designerCity@users.noreply.github.com>
2022-04-03 17:33:17 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,26 @@
-// response 받아와서 쓰는 함수들을 정리할 것이다. 
+const BASE_URL = 'https://learn.codeit.kr/api'
 
-export async function getList({ order = '', cursor = '', limit }) {
-  const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
-  const response = await fetch(`https://learn.codeit.kr/api/foods?${query}`);
-  const body = await response.json();
-  return body;
+// response 받아와서 쓰는 함수들을 정리할 것이다. 
+export async function getList({ order = '', cursor = '', limit = 5, search = '' }) {
+    const query = `order=${order}&cursor=${cursor}&limit=${limit}&search=${search}`;
+    const response = await fetch(`${BASE_URL}/foods?${query}`);
+    if (!response.ok) {
+        throw new Error('데이터를 불러오는데 실패했습니다.');
+    }
+    const body = await response.json();
+    return body;
 }
 // request 를 받아서 오는 response 를 json data 로 전환하여, body 에 넘겨준다. 
+
+// api 에 POST request 를 보내는 함수
+export async function createFood(formData) {
+    const response = await fetch(`${BASE_URL}/foods`, {
+        method: 'POST',
+        body: formData,
+    });
+    if (!response.ok) {
+        throw new Error('데이터를 생성하는데 실패했습니다!');
+    }
+    const body = await response.json();
+    return body;
+}
